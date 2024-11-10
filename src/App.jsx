48ebe6b0b5d1440cdeaa94378b631e40c380eb65
Ankit Raj Kaushal <rajkaushalark@gmail.com>
2024-11-10 22:06:39 +0530

    add code to bundle images with build

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
 import Header from "./Header";
 import RedirectCard from "./RedirectCard";
+import nearbyLogo from "./assets/nearby-brand-logo-square.jpg";
+import portfolioImage from "./assets/portfolioImg.jpeg";
+import resumeImage from "./assets/resumeImg.jpg";
 
 function App() {
   const myData = [
     {
       id: 1,
       url: "https://arkankit.github.io/portfolio-ark/",
-      imageUrl: "./src/assets/portfolioImg.jpeg",
     },
     {
       id: 2,
       url: "https://arkankit.github.io/resume-ark/",
-      imageUrl: "./src/assets/resumeImg.jpg",
     },
     {
       id: 3,
       url: "https://spontaneous-axolotl-120710.netlify.app/",
-      imageUrl: "./src/assets/nearby-brand-logo-square.jpg",
     },
   ];
 
@@ -24,9 +24,9 @@ function App() {
     <div>
       <Header />
       <div className="cards-container">
-        <RedirectCard redirectLink = {myData[0].url} cardImage = {myData[0].imageUrl}/>
-        <RedirectCard redirectLink = {myData[1].url} cardImage = {myData[1].imageUrl}/>
-        <RedirectCard redirectLink = {myData[2].url} cardImage = {myData[2].imageUrl}/>
+        <RedirectCard redirectLink = {myData[0].url} cardImage = {portfolioImage}/>
+        <RedirectCard redirectLink = {myData[1].url} cardImage = {resumeImage}/>
+        <RedirectCard redirectLink = {myData[2].url} cardImage = {nearbyLogo}/>
       </div>
     </div>
   );
